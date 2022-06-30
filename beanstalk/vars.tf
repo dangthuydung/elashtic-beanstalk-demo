82241cc9eb96b1c05ff23be061ef6b6126218a96
Dang Thuy Dung <dangthuydung.1909@gmail.com>
2022-06-30 14:41:52 +0700

    fix code 5

--- beanstalk/vars.tf
+++ beanstalk/vars.tf
@@ -1,9 +1,7 @@
 variable "name_EB_app" {}
 variable "name_EB_env" {}
 variable "vpc_id" {}
-variable "subnet_public" {
-    type = string 
-}
+variable "subnet_public" {}
 variable "key_name" {}
 variable "solution_stack_name" {}
 variable "tier" {}
